8b4c4efa12b91c765ce8adeba7b43cf27511d7cc
adarshashaw <62458669+adarshashaw@users.noreply.github.com>
2020-04-24 11:02:57 +0530

    Adarsha | Adds year field in State & Country election

--- ShowPublicElections.php
+++ ShowPublicElections.php
@@ -33,9 +33,9 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
     $stmt3->close();
     $response['validAuth']=true;
 
-    $stmt=$conn->prepare("SELECT Country_Election.id, Country_Election.name , Country_Election.status FROM Country_Election");
+    $stmt=$conn->prepare("SELECT Country_Election.id, Country_Election.name, Country_Election.status, Country_Election.year FROM Country_Election");
     $stmt->execute();
-    $stmt->bind_result($electionId, $electionName, $status);
+    $stmt->bind_result($electionId, $electionName, $status, $year);
 
     $election=array();
 
@@ -46,25 +46,27 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
         $temp['name']=$electionName;
         $temp['type']="LOK SABHA";
         $temp['status']=$status;
+        $temp['year']=$year;
         array_push($election, $temp);
         $response['success']=true;
     }
     $stmt->close();
 	
 	
-	$stmt2=$conn->prepare("SELECT State_Election.id, State_Election.name, State_Election.type, State_Election.status FROM State_Election WHERE State_Election.type='VIDHAN SABHA'");
+	$stmt2=$conn->prepare("SELECT State_Election.id, State_Election.name, State_Election.type, State_Election.status, State_Election.year FROM State_Election WHERE State_Election.type='VIDHAN SABHA'");
 	$stmt2->execute();
-	$stmt2->bind_result($electionId, $electionName, $type, $status);
+	$stmt2->bind_result($electionId, $electionName, $type, $status, $year);
 	
 	while($stmt2->fetch())
-    	{
-		$temp2=array();
-		$temp2['electionId']=$electionId;
-		$temp2['name']=$electionName;
-		$temp2['type']=$type;
-		$temp2['status']=$status;
-		array_push($election, $temp2);
-		$response['success']=true;
+    {
+        $temp2=array();
+        $temp2['electionId']=$electionId;
+        $temp2['name']=$electionName;
+        $temp2['type']=$type;
+        $temp2['status']=$status;
+        $temp2['year']=$year;
+        array_push($election, $temp2);
+        $response['success']=true;
 	}
 	$stmt2->close();
 	
