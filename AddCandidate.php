0db2aa4037e0680f440ba0b067461310da98c2ba
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-04-27 09:37:47 +0530

    Arnab | Adds logged in booth validation

--- AddCandidate.php
+++ AddCandidate.php
@@ -16,6 +16,7 @@ $electionId=$_POST["electionId"];
 $constituencyName=$_POST["constituencyName"];
 $partyName=$_POST["partyName"];
 $img=$_POST["img"];
+$boothId=$_POST["boothId"];
 
 
 $key_name="post_auth_key";
@@ -24,6 +25,7 @@ $key_name="post_auth_key";
 $response=array();
 $response['success']=false;
 $response['validAuth']=false;
+$response['validBooth']=false;
 $response['validElection']=false;
 $response['validConstituency']=false;
 $response['validParty']=false;
@@ -41,10 +43,10 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
 {
     $stmt3->close();
     $response['validAuth']=true;
-
-    $stmt=$conn->prepare("SELECT COUNT(name) FROM Party WHERE name=?");
-
-    $stmt->bind_param("s", $partyName);
+    
+    
+    $stmt=$conn->prepare("SELECT COUNT(booth_id) FROM Booth WHERE booth_id=? AND status=1");
+    $stmt->bind_param("s", $boothId);
     $stmt->execute();
     $stmt->bind_result($count);
 
@@ -52,63 +54,76 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
     {
         $count=-1;
         $stmt->close();
-        $response['validParty']=true;
+        $response['validBooth']=true;
 
-        $stmt=$conn->prepare("SELECT state_code, phase_code FROM Pub_Govt_Election WHERE id=? AND status=0");
-        $stmt->bind_param("d", $electionId);
+        $stmt=$conn->prepare("SELECT COUNT(name) FROM Party WHERE name=?");
+
+        $stmt->bind_param("s", $partyName);
         $stmt->execute();
-        $stmt->bind_result($stateCode, $phaseCode);
+        $stmt->bind_result($count);
 
-        if($stmt->fetch())
-        {        
+        if($stmt->fetch() && $count==1)
+        {
+            $count=-1;
             $stmt->close();
-            $response['validElection']=true;
+            $response['validParty']=true;
 
-            $stmt=$conn->prepare("SELECT COUNT(name) FROM Constituency WHERE state_code=? AND phase_code=? AND name=?");
-            $stmt->bind_param("sss", $stateCode, $phaseCode, $constituencyName);
+            $stmt=$conn->prepare("SELECT state_code, phase_code FROM Pub_Govt_Election WHERE id=? AND status=0");
+            $stmt->bind_param("d", $electionId);
             $stmt->execute();
-            $stmt->bind_result($count);
-            
-            if($stmt->fetch() && $count==1)
-            {
-                $count=-1;
-                $stmt->close();
-                $response['validConstituency']=true;
+            $stmt->bind_result($stateCode, $phaseCode);
 
-                $stmt=$conn->prepare("SELECT COUNT(id) FROM Candidate WHERE election_id=? AND constituency_name=? AND party_name=?");
-                $stmt->bind_param("dss", $electionId, $constituencyName, $partyName);
+            if($stmt->fetch())
+            {        
+                $stmt->close();
+                $response['validElection']=true;
+
+                $stmt=$conn->prepare("SELECT COUNT(name) FROM Constituency WHERE state_code=? AND phase_code=? AND name=?");
+                $stmt->bind_param("sss", $stateCode, $phaseCode, $constituencyName);
                 $stmt->execute();
                 $stmt->bind_result($count);
 
-                if($stmt->fetch() && $count==0)
+                if($stmt->fetch() && $count==1)
                 {
                     $count=-1;
                     $stmt->close();
-                    $response['validCandidate']=true;
+                    $response['validConstituency']=true;
 
-                    $stmt=$conn->prepare("INSERT INTO Candidate (name, election_id, constituency_name, party_name, img) VALUES (?, ?, ?, ?, ?)");
-                    $stmt->bind_param("sdsss", $name, $electionId, $constituencyName, $partyName, $img);                
-                    
-                    if($stmt->execute())
+                    $stmt=$conn->prepare("SELECT COUNT(id) FROM Candidate WHERE election_id=? AND constituency_name=? AND party_name=?");
+                    $stmt->bind_param("dss", $electionId, $constituencyName, $partyName);
+                    $stmt->execute();
+                    $stmt->bind_result($count);
+
+                    if($stmt->fetch() && $count==0)
                     {
+                        $count=-1;
                         $stmt->close();
+                        $response['validCandidate']=true;
 
-                        $stmt=$conn->prepare("SELECT id FROM Candidate WHERE election_id=? AND party_name=? AND constituency_name=?");
-                        $stmt->bind_param("dss", $electionId, $partyName, $constituencyName);
-                        $stmt->execute();
-                        $stmt->bind_result($candidateId);
-                        $stmt->fetch();
-                        $stmt->close();
+                        $stmt=$conn->prepare("INSERT INTO Candidate (name, election_id, constituency_name, party_name, img) VALUES (?, ?, ?, ?, ?)");
+                        $stmt->bind_param("sdsss", $name, $electionId, $constituencyName, $partyName, $img);                
 
-                        $response['candidateId']=$candidateId;
-                        $response['success']=true;
+                        if($stmt->execute())
+                        {
+                            $stmt->close();
+
+                            $stmt=$conn->prepare("SELECT id FROM Candidate WHERE election_id=? AND party_name=? AND constituency_name=?");
+                            $stmt->bind_param("dss", $electionId, $partyName, $constituencyName);
+                            $stmt->execute();
+                            $stmt->bind_result($candidateId);
+                            $stmt->fetch();
+                            $stmt->close();
+
+                            $response['candidateId']=$candidateId;
+                            $response['success']=true;
+
+                        }
 
                     }
-
                 }
-            }
 
-            
+
+            }
         }
     }
     
