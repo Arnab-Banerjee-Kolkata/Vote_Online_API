f91b856a6022fdce7f868c5ba5f0766a6eac8f02
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-04-27 09:06:15 +0530

    Arnab | Logged in booth to validate OTP

--- ValidateOtp.php
+++ ValidateOtp.php
@@ -25,8 +25,11 @@ $key_name="post_auth_key";
 $response=array();
 $response['success']=false;
 $response['validAuth']=false;
+$response['validCategory']=false;
+$response['validBooth']=false;
 $response['validStatus']=false;
 $response['validApproval']=false;
+$response['validOtp']=false;
 $response['boothOtp']="-1";
 
 $stmt3=$conn->prepare("SELECT key_value FROM Authenticate_Keys WHERE name = ?");
@@ -42,78 +45,126 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
 {
     $stmt3->close();
     $response['validAuth']=true;
-
-
-    if($category=="voter")          //Called from RPD
+    
+    if($category=="voter" || $category=="booth")
     {
-        $stmt=$conn->prepare("SELECT COUNT(aadhaar_no) FROM Govt_Vote_Status WHERE election_id=? AND aadhaar_no=?");
-        $stmt->bind_param("ss",$electionId, $aadhaarNo);
-        $stmt->execute();
-        $stmt->bind_result($vts);
-        if($stmt->fetch() && $vts==0)
+        $response['validCategory']=true;
+
+        if($category=="voter")          //Called from RPD
         {
-            $response['validStatus']=true;
-        }
-        $stmt->close();
-
-
-        $stmt = $conn->prepare("SELECT voter_otp FROM Credentials WHERE aadhaar_no=?");
-        $stmt->bind_param("s", $aadhaarNo);
-        $stmt->execute();
-        $stmt->bind_result($otp2);
-        if($stmt->fetch() && $otp1==$otp2)
-        {            
-            $stmt->close();
-
-            $stmt=$conn->prepare("SELECT otp FROM Booth WHERE booth_id=?");
+            $stmt=$conn->prepare("SELECT COUNT(booth_id) FROM Booth WHERE booth_id=? AND status=1");
             $stmt->bind_param("s", $boothId);
             $stmt->execute();
-            $stmt->bind_result($response['boothOtp']);
-            $stmt->fetch();
-            $stmt->close();
+            $stmt->bind_result($count);
+            
+            if($stmt->fetch() && $count==1)
+            {
+                $count=-1;
+                $stmt->close();
+                $response['validBooth']=true;
+            
+            
+                $stmt=$conn->prepare("SELECT COUNT(aadhaar_no) FROM Govt_Vote_Status WHERE election_id=? AND aadhaar_no=?");
+                $stmt->bind_param("ss",$electionId, $aadhaarNo);
+                $stmt->execute();
+                $stmt->bind_result($vts);
+                if($stmt->fetch() && $vts==0)
+                {
+                    $response['validStatus']=true;                
+                    $stmt->close();
+                    $vts=-1;
 
 
-            $response['success']=true;            
+                    $stmt = $conn->prepare("SELECT voter_otp FROM Credentials WHERE aadhaar_no=?");
+                    $stmt->bind_param("s", $aadhaarNo);
+                    $stmt->execute();
+                    $stmt->bind_result($otp2);
+                    if($stmt->fetch() && $otp1==$otp2)
+                    {            
+                        $stmt->close();
+                        $response['validOtp']=true;
+
+                        $stmt=$conn->prepare("SELECT otp FROM Booth WHERE booth_id=? AND status=1");
+                        $stmt->bind_param("s", $boothId);
+                        $stmt->execute();
+                        $stmt->bind_result($response['boothOtp']);
+                        $stmt->fetch();
+                        $stmt->close();
+
+
+                        $response['success']=true;            
+                    }
+                    else
+                        $stmt->close();
+                }            
+                else
+                {
+                    $stmt->close();
+                }
+                $otp=rand(1000, 9999);
+                $stmt2=$conn->prepare("UPDATE Credentials SET voter_otp=? WHERE aadhaar_no=?");
+                $stmt2->bind_param("ss", $otp, $aadhaarNo);
+                $stmt2->execute();
+
+                $stmt2->close();
+            }
         }
-        else
-            $stmt->close();
-
-        $otp=rand(1000, 9999);
-        $stmt2=$conn->prepare("UPDATE Credentials SET voter_otp=? WHERE aadhaar_no=?");
-        $stmt2->bind_param("ss", $otp, $aadhaarNo);
-        $stmt2->execute();
-        
-        $stmt2->close();
-    }
-    else if($category=="booth")         //Called from voting device
-    {
-        $stmt=$conn->prepare("SELECT COUNT(aadhaar_no) FROM Govt_Approval WHERE election_id=? AND aadhaar_no=?");
-        $stmt->bind_param("ss",$electionId, $aadhaarNo);
-        $stmt->execute();
-        $stmt->bind_result($apr);
-        if($stmt->fetch() && $apr!=0)
+        else if($category=="booth")         //Called from voting device
         {
-            $response['validApproval']=true;
-        }
-        $stmt->close();
+            $stmt=$conn->prepare("SELECT COUNT(booth_id) FROM Booth WHERE booth_id=? AND status=1");
+            $stmt->bind_param("s", $boothId);
+            $stmt->execute();
+            $stmt->bind_result($count);
+            
+            if($stmt->fetch() && $count==1)
+            {
+                $count=-1;
+                $stmt->close();
+                $response['validBooth']=true;
+            
+            
+                $stmt=$conn->prepare("SELECT COUNT(aadhaar_no) FROM Govt_Approval WHERE election_id=(SELECT id FROM Pub_Govt_Election WHERE id=? AND status=1) AND aadhaar_no=?");
+                $stmt->bind_param("ss",$electionId, $aadhaarNo);
+                $stmt->execute();
+                $stmt->bind_result($apr);
+                if($stmt->fetch() && $apr!=0)
+                {
+                    $response['validApproval']=true;
+                    $stmt->close();
+                    $apr=-1;
+                
+                
 
-        $stmt = $conn->prepare("SELECT otp FROM Booth WHERE booth_id=?");
-        $stmt->bind_param("s", $boothId);
-        $stmt->execute();
-        $stmt->bind_result($otp2);
-        if($stmt->fetch() && $otp1==$otp2 && $response['validApproval'])
-        {                     
-            $response['success']=true;             
-        }
-        $stmt->close();
-        $otp=rand(1000, 9999);
-        $stmt2=$conn->prepare("UPDATE Booth SET otp=? WHERE booth_id=?");
-        $stmt2->bind_param("ss", $otp, $boothId);
-        $stmt2->execute();
-        
-        $stmt2->close();
+                    $stmt = $conn->prepare("SELECT otp FROM Booth WHERE booth_id=? AND status=1");
+                    $stmt->bind_param("s", $boothId);
+                    $stmt->execute();
+                    $stmt->bind_result($otp2);
+                    if($stmt->fetch() && $otp1==$otp2)
+                    {                     
+                        $stmt->close();
+                        $response['validOtp']=true;                    
+                        
+                        
+                        $response['success']=true;             
+                    }    
+                    else
+                    {
+                        $stmt->close();
+                    }                
+                }
+                else
+                {
+                    $stmt->close();
+                }
+                $otp=rand(1000, 9999);
+                $stmt2=$conn->prepare("UPDATE Booth SET otp=? WHERE booth_id=?");
+                $stmt2->bind_param("ss", $otp, $boothId);
+                $stmt2->execute();
 
-    }    
+                $stmt2->close();
+            }
+        }
+    }
 }
 $conn->close();
 
