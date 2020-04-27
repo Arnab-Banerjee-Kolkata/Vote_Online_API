46babb0763d50d4643dfec3bed4c1a58632c4674
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-04-27 11:45:26 +0530

    Arnab | Adds file to get random key set

---
+++ GetRandomKey.php
@@ -0,0 +1,65 @@
+<?php
+
+include 'Credentials.php';
+include 'EncryptionKeys.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$postAuthKey1=$_POST["postAuthKey"];
+$boothId=$_POST["boothId"];
+
+
+$key_name="post_auth_key";
+
+
+$response=array();
+$response['success']=false;
+$response['validAuth']=false;
+$response['validBooth']=false;
+$response['setNo']=-1;
+$response['keySet']=array();
+
+$stmt3=$conn->prepare("SELECT key_value FROM Authenticate_Keys WHERE name =?");
+$stmt3->bind_param("s", $key_name);
+
+$stmt3->execute();
+$stmt3->bind_result($postAuthKey2);
+
+if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
+{
+    $stmt3->close();
+    $response['validAuth']=true;
+
+    
+    $stmt=$conn->prepare("SELECT COUNT(booth_id) FROM Booth WHERE booth_id=? AND status=1");
+    $stmt->bind_param("s", $boothId);
+    $stmt->execute();
+    $stmt->bind_result($count);
+
+    if($stmt->fetch() && $count==1)
+    {
+        $count=-1;
+        $stmt->close();
+        $response['validBooth']=true;
+
+        $KEY_SET_SIZE=sizeof($keySet);
+        $setNo=mt_rand(0,$KEY_SET_SIZE-1);
+        $response['setNo']=$setNo;
+        $response['keySet']=$keySet[$setNo];
+        
+        $response['success']=true;
+
+    }
+}
+$conn->close();
+
+echo json_encode($response);
+
+
+?>
