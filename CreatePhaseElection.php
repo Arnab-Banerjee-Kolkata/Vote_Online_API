3c02e892317a8eaafb808a78b86d09432dc396f8
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-04-27 10:11:45 +0530

    Arnab | Updates with logged in booth validation

--- CreatePhaseElection.php
+++ CreatePhaseElection.php
@@ -13,6 +13,7 @@ if ($conn->connect_error) {
 $postAuthKey1=$_POST["postAuthKey"];
 $stateElectionId=$_POST["stateElectionId"];
 $phaseCode=$_POST["phaseCode"];
+$boothId=$_POST["boothId"];
 
 
 $key_name="post_auth_key";
@@ -21,6 +22,7 @@ $key_name="post_auth_key";
 $response=array();
 $response['success']=false;
 $response['validAuth']=false;
+$response['validBooth']=false;
 $response['validStateElection']=false;
 $response['validPhaseCode']=false;
 $response['validElection']=false;
@@ -39,66 +41,79 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
 {
     $stmt3->close();
     $response['validAuth']=true;
-
-    $stmt=$conn->prepare("SELECT COUNT(id), state_code, type from State_Election WHERE id=?");
-
-    $stmt->bind_param("d", $stateElectionId);
-    $stmt->execute();
-    $stmt->bind_result($count, $stateCode, $type);
     
-    if(strlen($type)==0)
-        $type="LOK SABHA";
+    
+    $stmt=$conn->prepare("SELECT COUNT(booth_id) FROM Booth WHERE booth_id=? AND status=1");
+    $stmt->bind_param("s", $boothId);
+    $stmt->execute();
+    $stmt->bind_result($count);
 
     if($stmt->fetch() && $count==1)
     {
-        $stmt->close();
-        $response['validStateElection']=true;
         $count=-1;
-        
-        
-        $stmt=$conn->prepare("SELECT COUNT(code) FROM Phase WHERE code=? AND state_code=? AND type=?");
-        $stmt->bind_param("sss", $phaseCode, $stateCode, $type);
+        $stmt->close();
+        $response['validBooth']=true;
+    
+
+        $stmt=$conn->prepare("SELECT COUNT(id), state_code, type from State_Election WHERE id=?");
+
+        $stmt->bind_param("d", $stateElectionId);
         $stmt->execute();
-        $stmt->bind_result($count);
-        
+        $stmt->bind_result($count, $stateCode, $type);
+
+        if(strlen($type)==0)
+            $type="LOK SABHA";
+
         if($stmt->fetch() && $count==1)
         {
             $stmt->close();
+            $response['validStateElection']=true;
             $count=-1;
-            $response['validPhaseCode']=true;
-        
-        
-            $stmt=$conn->prepare("SELECT COUNT(id) FROM Pub_Govt_Election WHERE state_election_id=? AND phase_code=?");
-            $stmt->bind_param("ds", $stateElectionId, $phaseCode);
+
+
+            $stmt=$conn->prepare("SELECT COUNT(code) FROM Phase WHERE code=? AND state_code=? AND type=?");
+            $stmt->bind_param("sss", $phaseCode, $stateCode, $type);
             $stmt->execute();
             $stmt->bind_result($count);
-            
-            if($stmt->fetch() && $count==0)
+
+            if($stmt->fetch() && $count==1)
             {
                 $stmt->close();
                 $count=-1;
-                $response['validElection']=true;            
-        
+                $response['validPhaseCode']=true;
 
-                $stmt=$conn->prepare("INSERT INTO Pub_Govt_Election (state_code, phase_code, type, status, state_election_id) VALUES (?, ?, ?, 0, ?)");
-                $stmt->bind_param("sssd", $stateCode, $phaseCode, $type, $stateElectionId);
-                $stmt->execute();
 
-                $stmt->close();
-
-                $stmt=$conn->prepare("SELECT id FROM Pub_Govt_Election WHERE state_election_id=? AND phase_Code=?");
+                $stmt=$conn->prepare("SELECT COUNT(id) FROM Pub_Govt_Election WHERE state_election_id=? AND phase_code=?");
                 $stmt->bind_param("ds", $stateElectionId, $phaseCode);
                 $stmt->execute();
-                $stmt->bind_result($response['electionId']);
-                $stmt->fetch();
-                $stmt->close();
+                $stmt->bind_result($count);
 
-                if($response['electionId']!=-1)
-                    $response['success']=true;
+                if($stmt->fetch() && $count==0)
+                {
+                    $stmt->close();
+                    $count=-1;
+                    $response['validElection']=true;            
+
+
+                    $stmt=$conn->prepare("INSERT INTO Pub_Govt_Election (state_code, phase_code, type, status, state_election_id) VALUES (?, ?, ?, 0, ?)");
+                    $stmt->bind_param("sssd", $stateCode, $phaseCode, $type, $stateElectionId);
+                    $stmt->execute();
+
+                    $stmt->close();
+
+                    $stmt=$conn->prepare("SELECT id FROM Pub_Govt_Election WHERE state_election_id=? AND phase_Code=?");
+                    $stmt->bind_param("ds", $stateElectionId, $phaseCode);
+                    $stmt->execute();
+                    $stmt->bind_result($response['electionId']);
+                    $stmt->fetch();
+                    $stmt->close();
+
+                    if($response['electionId']!=-1)
+                        $response['success']=true;
+                }
             }
         }
     }
-    
 
     
 }
