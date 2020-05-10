e27cfb6072327b8c3ea0d2f8a230725451c4aabc
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-10 09:50:42 +0530

    Arnab | Adds file to show pending country elections

---
+++ ShowCountryElections.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'Credentials.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$postAuthKey1=$_POST["postAuthKey"];
+
+
+$key_name="post_auth_key";
+
+
+$response=array();
+$response['success']=false;
+$response['validAuth']=false;
+
+$stmt3=$conn->prepare("SELECT key_value FROM Authenticate_Keys WHERE name =?");
+$stmt3->bind_param("s", $key_name);
+
+$stmt3->execute();
+$stmt3->bind_result($postAuthKey2);
+
+
+
+if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
+{
+    $stmt3->close();
+    $response['validAuth']=true;
+
+    $stmt=$conn->prepare("SELECT Country_Election.id, Country_Election.name, Country_Election.year FROM Country_Election WHERE status=0");
+    $stmt->execute();
+    $stmt->bind_result($electionId, $electionName, $year);
+
+    $election=array();
+
+    while($stmt->fetch())
+    {
+        $temp=array();
+        $temp['electionId']=$electionId;
+        $temp['name']=$electionName;
+        $temp['year']=$year;
+        array_push($election, $temp);
+        $response['success']=true;
+    }
+    $stmt->close();
+	
+	
+    $response['elections']=$election;
+
+}
+$conn->close();
+
+echo json_encode($response);
+
+?>
