6f6ab7f40a2c7aed3841d033d4a02ae83b9559ec
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-12 10:28:01 +0530

    Arnab | Checks escape character

--- AdminLogout.php
+++ AdminLogout.php
@@ -11,8 +11,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$postAuthKey1=$_POST["postAuthKey"];
-$adminId=$_POST["adminId"];
+$postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
+$adminId=$conn->real_escape_string($_POST["adminId"]);
 
 $key_name="post_auth_key";
 checkServerIp($INTERNAL_AUTH_KEY);
