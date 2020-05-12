d52b00457d2cce52ebb87ca7061879d372b0ee87
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-12 22:22:52 +0530

    Arnab | Fixes bug

--- ConstituencyWiseResult.php
+++ ConstituencyWiseResult.php
@@ -80,10 +80,10 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
                 $stmt->close();
                 $count=-1;
 
-                $stmt=$conn->prepare("SELECT Constituency_Result.constituency_name, Candidate.name, Candidate.party_name, Party.symbol, Constituency_Result.winner_vote_count FROM Constituency_Result, Candidate, Party WHERE Constituency_Result.state_election_id=? AND Constituency_Result.winner_candidate_id=Candidate.id AND Party.name=Candidate.party_name");
+                $stmt=$conn->prepare("SELECT Constituency_Result.constituency_name, Candidate.name, Candidate.party_name, Party.symbol, Constituency_Result.winner_vote_count, Candidate.img FROM Constituency_Result, Candidate, Party WHERE Constituency_Result.state_election_id=? AND Constituency_Result.winner_candidate_id=Candidate.id AND Party.name=Candidate.party_name");
                 $stmt->bind_param("d", $electionId);
                 $stmt->execute();
-                $stmt->bind_result($constituencyName, $candidateName, $partyName, $partySymbol, $voteCount);
+                $stmt->bind_result($constituencyName, $candidateName, $partyName, $partySymbol, $voteCount, $candidateImg);
 
                 $results=array();
 
@@ -92,6 +92,7 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
                     $result=array();
                     $result['constituencyName']=$constituencyName;
                     $result['candidateName']=$candidateName;
+                    $result['candidateImage']=$candidateImg;
                     $result['partyName']=$partyName;
                     $result['partySymbol']=$partySymbol;
                     $result['voteCount']=$voteCount;
