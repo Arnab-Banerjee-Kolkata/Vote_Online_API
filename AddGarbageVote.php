fd8910b322faf3980621653f03feb318bfc32be5
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-12 18:00:18 +0530

    Adarsha | Fixes bugs

--- AddGarbageVote.php
+++ AddGarbageVote.php
@@ -2,7 +2,7 @@
 
 function addGarbageVote($internalAuthKey,$constName,$phaseId)
 {
-	include 'StoreApproval.php';
+	include 'Credentials.php';
 	
 	// Create connection
 	$vbConn = new mysqli($vbServerName, $vbUserName, $vbPassword, $vbDbName);
@@ -12,6 +12,10 @@ function addGarbageVote($internalAuthKey,$constName,$phaseId)
     die("Connection failed: " . $vbConn->connect_error);
 }
 
+	$internalAuthKey=$vbConn->real_escape_string($internalAuthKey);
+	$constName=$vbConn->real_escape_string($constName);
+	$phaseId=$vbConn->real_escape_string($phaseId);
+	
 	$response=array();
 	$response['success']=false;
 	$response['validInternalAuth2']=false;
@@ -19,10 +23,10 @@ function addGarbageVote($internalAuthKey,$constName,$phaseId)
 	
 	if($internalAuthKey==$INTERNAL_AUTH_KEY)
     	{
-        	$response['validInternalAuth']=true;
+        	$response['validInternalAuth2']=true;
 			
 			$stmt=$vbConn->prepare("SELECT COUNT(en_vote),count FROM Govt_Vote WHERE phase_election_id=? AND en_vote=? AND constituency_name=?");
-			$stmt->bind_param("ds",$phaseId,$garbage,$constName);
+			$stmt->bind_param("dss",$phaseId,$garbage,$constName);
 			$stmt->execute();
 			$stmt->bind_result($count1,$countInTable);
 			$stmt->fetch();
@@ -32,22 +36,22 @@ function addGarbageVote($internalAuthKey,$constName,$phaseId)
 			{
 				$count1=-1;
 				
-				$stmt2=$vbConn->prepare("UPDATE Govt_Vote SET count=? WHERE phase_election_id=? AND en_vote=? AND constituency_name=?")
-				$stmt2->bind_param("ddss",($countInTable+1),$phaseId,$garbage,$constName);
+				$stmt2=$vbConn->prepare("UPDATE Govt_Vote SET count=? WHERE phase_election_id=? AND en_vote=? AND constituency_name=?");
+				$stmt2->bind_param("ddss",++$countInTable,$phaseId,$garbage,$constName);
 				$stmt2->execute();
-				$stmt2->bind_result();
-				$stmt2->fetch();
+                		$stmt2->fetch();
 				$stmt2->close();
 				
 				$response['success']=true;
 			}
 			else
 			{
-				$count=-2;
+				$count1=-2;
 
 				$stmt3=$vbConn->prepare("INSERT INTO Govt_Vote (phase_election_id,en_vote,constituency_name,count) VALUES (?,?,?,1)");
 				$stmt3->bind_param("dss",$phaseId,$garbage,$constName);
 				$stmt3->execute();
+                		$stmt3->fetch();
 				$stmt3->close();
 				
 				$response['success']=true;
