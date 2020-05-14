fed890a2617ce77d7f30c3458f56a5bbeaf82fc7
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-14 22:41:23 +0530

    Adarsha | Adds validation of Place

--- BoothAddress.php
+++ BoothAddress.php
@@ -23,6 +23,7 @@ $response=array();
 
 $response['success']=false;
 $response['validAuth']=false;
+$response['validPlace']=false;
 
 $stmt=$conn->prepare("SELECT key_value FROM Authenticate_Keys WHERE name=?");
 $stmt->bind_param("s",$key_name);
@@ -34,28 +35,41 @@ if($stmt->fetch() && $postAuthKey1==$postAuthKey2)
 	$stmt->close();
 	$response['validAuth']=true;
 	
-	$stmt2=$conn->prepare("SELECT locality,address,land_mark FROM Booth WHERE place=? AND (status=0 OR status=1) ORDER BY locality");
+	$stmt2=$conn->prepare("SELECT COUNT(place) FROM Booth WHERE place=?  AND (status=0 OR status=1)");
 	$stmt2->bind_param("s",$place);
 	$stmt2->execute();
-	$stmt2->bind_result($area,$address,$landmark);
+	$stmt2->bind_result($count);
 	
-	$allBoothsInPlace=array();
-	
-	while($stmt2->fetch())
+	if($stmt2->fetch() && $count>=1)
 	{
-		$boothAddress=array();
-		$boothAddress['area']=$area;
-		$boothAddress['address']=$address;
-		$boothAddress['landmark']=$landmark;
-		
-		array_push($allBoothsInPlace,$boothAddress);
-	}
-    
-	$stmt2->close();
-	
-	$response['allBoothsInPlace']=$allBoothsInPlace;
-	$response['success']=true;
+		$stmt2->close();
+		$response['validPlace']=true;
 
+		$stmt3=$conn->prepare("SELECT locality,address,land_mark FROM Booth WHERE place=? AND (status=0 OR status=1) ORDER BY locality");
+		$stmt3->bind_param("s",$place);
+		$stmt3->execute();
+		$stmt3->bind_result($area,$address,$landmark);
+	
+		$allBoothsInPlace=array();
+	
+		while($stmt3->fetch())
+		{
+			$boothAddress=array();
+			$boothAddress['area']=$area;
+			$boothAddress['address']=$address;
+			$boothAddress['landmark']=$landmark;
+		
+			array_push($allBoothsInPlace,$boothAddress);
+		}
+    
+		$stmt3->close();
+	
+		$response['allBoothsInPlace']=$allBoothsInPlace;
+		$response['success']=true;
+	}
+	else
+		$stmt2->close();
+	
 }
 
 else
