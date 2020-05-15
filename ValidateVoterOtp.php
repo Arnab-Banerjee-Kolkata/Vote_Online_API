270756ce66f6d6f27926ff32a5f4d2b6329ec78b
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-15 21:03:38 +0530

    Arnab | Fixes bugs

--- ValidateVoterOtp.php
+++ ValidateVoterOtp.php
@@ -28,7 +28,6 @@ $response=array();
 $response['validAuth']=false;
 $response['validBooth']=false;
 $response['validAadhaar']=false;
-$response['validVoteStatus']=false;
 $response['validApproval']=false;
 $response['validOtp']=false;
 
@@ -64,78 +63,65 @@ if($stmt->fetch() && $postAuthKey1==$postAuthKey2)
 			$count2=-1;
 			$response['validAadhaar']=true;
 			
-			$stmt4=$conn->prepare("SELECT COUNT(aadhaar_no) FROM  Govt_Vote_Status WHERE aadhaar_no=?");
-			$stmt4->bind_param("s",$aadhaarNo);
-			$stmt4->execute();
-			$stmt4->bind_result($count3);
 			
-			if($stmt4->fetch() && $count3==0)
-			{
-				$stmt4->close();
-				$count3=-1;
-				$response['validVoteStatus']=true;
 				
-				$stmt5=$conn->prepare("SELECT COUNT(booth_id) FROM Govt_Approval WHERE booth_id=?");
-				$stmt5->bind_param("s",$boothId);
-				$stmt5->execute();
-				$stmt5->bind_result($count4);
-				
-				if($stmt5->fetch() && $count4==0)
-				{
-					$stmt5->close();
-					$count4=-1;
-					$response['validApproval']=true;
-					
-					$stmt6=$conn->prepare("SELECT voter_otp FROM Credentials WHERE aadhaar_no=?");
-					$stmt6->bind_param("s",$aadhaarNo);
-					$stmt6->execute();
-					$stmt6->bind_result($otp);
+            $stmt5=$conn->prepare("SELECT COUNT(booth_id) FROM Govt_Approval WHERE booth_id=?");
+            $stmt5->bind_param("s",$boothId);
+            $stmt5->execute();
+            $stmt5->bind_result($count4);
+            
+            if($stmt5->fetch() && $count4==0)
+            {
+                $stmt5->close();
+                $count4=-1;
+                $response['validApproval']=true;
+                
+                $stmt6=$conn->prepare("SELECT voter_otp FROM Credentials WHERE aadhaar_no=?");
+                $stmt6->bind_param("s",$aadhaarNo);
+                $stmt6->execute();
+                $stmt6->bind_result($otp);
 
-                    $voterOtp=encrypt($INTERNAL_AUTH_KEY, $voterOtp, $keySet[8]);
-					
-					if($stmt6->fetch() && $voterOtp==$otp)
-					{
-						$stmt6->close();
-						$response['validOtp']=true;
+                $voterOtp=encrypt($INTERNAL_AUTH_KEY, $voterOtp, $keySet[8]);
+                
+                if($stmt6->fetch() && $voterOtp==$otp)
+                {
+                    $stmt6->close();
+                    $response['validOtp']=true;
 
-                        //return booth OTP
-                        $boothOtp=decrypt($INTERNAL_AUTH_KEY, $boothOtp, $keySet[8]);                        
+                    //return booth OTP
+                    $boothOtp=decrypt($INTERNAL_AUTH_KEY, $boothOtp, $keySet[8]);                        
 
-                        //StoreApproval
-                        $response['returnValue']=storeApproval($conn,$INTERNAL_AUTH_KEY,$aadhaarNo,$electionId,$type,$boothId);
-                        if($response['returnValue']['garbageAndVoted']['success'])
-                        {
-                            $response['boothOtp']=$boothOtp;
-                        }
-					}
-					else
-					{
-						$stmt6->close();
-					}
-					
-					$times=mt_rand(1,12);
-					while($times>0)
-					{
-						$newOtp=mt_rand(1000, mt_rand(1001,9999));
-						$times--;
-					}
+                    //StoreApproval
+                    $response['returnValue']=storeApproval($conn,$INTERNAL_AUTH_KEY,$aadhaarNo,$electionId,$type,$boothId);
+                    if($response['returnValue']['garbageVoted']['success'])
+                    {
+                        $response['boothOtp']=$boothOtp;
+                    }
+                }
+                else
+                {
+                    $stmt6->close();
+                }
+                
+                $times=mt_rand(1,12);
+                while($times>0)
+                {
+                    $newOtp=mt_rand(1000, mt_rand(1001,9999));
+                    $times--;
+                }
 
-                    $newOtp=encrypt($INTERNAL_AUTH_KEY, $newOtp, $keySet[8]);
-		
-					$stmt7=$conn->prepare("UPDATE Credentials SET voter_otp=? WHERE aadhaar_no=?");
-					$stmt7->bind_param("ss",$newOtp,$aadhaarNo);
-					$stmt7->execute();
-					$stmt7->close();
-				}
-				else
-				{
-					$stmt5->close();
-				}
-			}
-			else
-			{
-				$stmt4->close();
-			}
+                $newOtp=encrypt($INTERNAL_AUTH_KEY, $newOtp, $keySet[8]);
+    
+                $stmt7=$conn->prepare("UPDATE Credentials SET voter_otp=? WHERE aadhaar_no=?");
+                $stmt7->bind_param("ss",$newOtp,$aadhaarNo);
+                $stmt7->execute();
+                $stmt7->close();
+            }
+            else
+            {
+                $stmt5->close();
+            }
+			
 		}
 		else
 		{
