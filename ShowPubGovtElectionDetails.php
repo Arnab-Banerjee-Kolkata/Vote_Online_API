0836ea12516c5a5ccfc983c870bf95d81a1cd426
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-15 21:44:05 +0530

    Arnab | Fixes bug

--- ShowPubGovtElectionDetails.php
+++ ShowPubGovtElectionDetails.php
@@ -3,7 +3,10 @@
 include 'Credentials.php';
 include 'Protection.php';
 
-//ini_set('display_errors', 1);
+foreach($_POST as $element)
+{
+    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,10 +16,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-foreach($_POST as $element)
-{
-    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
-}
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
 $electionId=$conn->real_escape_string($_POST["electionId"]);
