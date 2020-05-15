49358b56d11b4d4c164f6943687f54b9a54e638b
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-15 18:14:49 +0530

    Arnab | Fixes bug

--- BoothPlaces.php
+++ BoothPlaces.php
@@ -3,6 +3,10 @@
 include 'Credentials.php';
 include 'Protection.php';
 
+foreach($_POST as $element)
+{
+    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
+}
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -13,7 +17,6 @@ if ($conn->connect_error) {
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
 
-checkForbiddenPhrase($INTERNAL_AUTH_KEY, $postAuthKey1);
 
 $key_name="post_auth_key";
 
