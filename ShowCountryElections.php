28b59cbfa76fe198fa403fb1c081fcca9cc5d2bd
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-15 18:51:05 +0530

    Arnab | Fixes bug

--- ShowCountryElections.php
+++ ShowCountryElections.php
@@ -3,6 +3,11 @@
 include 'Credentials.php';
 include 'Protection.php';
 
+checkServerIp($INTERNAL_AUTH_KEY);
+foreach($_POST as $element)
+{
+    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
+}
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,12 +16,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-foreach($_POST as $element)
-{
-    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
-}
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
+$adminId=$conn->real_escape_string($_POST["adminId"]);
 
 
 $key_name="post_auth_key";
@@ -25,6 +27,7 @@ $key_name="post_auth_key";
 $response=array();
 $response['success']=false;
 $response['validAuth']=false;
+$response['validAdmin']=false;
 
 $stmt3=$conn->prepare("SELECT key_value FROM Authenticate_Keys WHERE name =?");
 $stmt3->bind_param("s", $key_name);
@@ -39,25 +42,38 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
     $stmt3->close();
     $response['validAuth']=true;
 
-    $stmt=$conn->prepare("SELECT Country_Election.id, Country_Election.name, Country_Election.year FROM Country_Election WHERE status=0");
+    $stmt=$conn->prepare("SELECT COUNT(id) FROM Admin_Credentials WHERE id=? AND status=1");
+    $stmt->bind_param("s", $adminId);
     $stmt->execute();
-    $stmt->bind_result($electionId, $electionName, $year);
-
-    $election=array();
-
-    while($stmt->fetch())
-    {
-        $temp=array();
-        $temp['electionId']=$electionId;
-        $temp['name']=$electionName;
-        $temp['year']=$year;
-        array_push($election, $temp);
-        $response['success']=true;
-    }
+    $stmt->bind_result($count);
+    $stmt->fetch();
     $stmt->close();
-	
-	
-    $response['elections']=$election;
+
+    if($count==1)
+    {
+        $response['validAdmin']=true;
+        $count=-1;
+
+        $stmt=$conn->prepare("SELECT Country_Election.id, Country_Election.name, Country_Election.year FROM Country_Election WHERE status=0");
+        $stmt->execute();
+        $stmt->bind_result($electionId, $electionName, $year);
+
+        $election=array();
+
+        while($stmt->fetch())
+        {
+            $temp=array();
+            $temp['electionId']=$electionId;
+            $temp['name']=$electionName;
+            $temp['year']=$year;
+            array_push($election, $temp);
+            $response['success']=true;
+        }
+        $stmt->close();
+        
+        
+        $response['elections']=$election;
+    }
 
 }
 $conn->close();
