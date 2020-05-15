8154ff07a989a7cab452d82d93e28dbb12aceb31
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-15 11:06:29 +0530

    Arnab | Send sms if voter has not voted

--- TestSms.php
+++ TestSms.php
@@ -58,6 +58,8 @@ foreach($_POST as $element)
 $aadhaarNo=$conn->real_escape_string($_POST["aadhaarNo"]);
 $smsAuthKey1=$conn->real_escape_string($_POST["smsAuthKey"]);
 $boothId=$conn->real_escape_string($_POST["boothId"]);
+$electionId=$conn->real_escape_string($_POST["electionId"]);
+$type=$conn->real_escape_string($_POST["type"]);
 
 //checkServerIp($INTERNAL_AUTH_KEY);
 
@@ -68,6 +70,8 @@ $response['success']=false;
 $response['validBooth']=false;
 $response['validAadhaar']=false;
 $response['validSmsAuth']=false;
+$response['validType']=false;
+$response['validVoter']=false;
 
 
 $stmt=$conn->prepare("SELECT COUNT(booth_id) FROM Booth WHERE booth_id=? AND status=1");
@@ -119,9 +123,67 @@ if($count==1)
                     {
                         $stmt3->close();
                         $response['validSmsAuth']=true;
-                    
-                        if(sendOTP($conn, $INTERNAL_AUTH_KEY, $countryCode, $regMobNo, $voterOTP, $API_KEY))
-                            $response['success']=true;
+
+                        $phaseElectionId="";
+                        if($type=="LOK SABHA")
+                        {
+                            $response['validType']=true;
+                            $stmt=$conn->prepare("SELECT id FROM Pub_Govt_Election WHERE status=1 AND phase_code = (
+                                SELECT phase_code FROM Constituency WHERE name=(
+                                    SELECT lok_sabha_constituency FROM Govt_DB WHERE aadhaar_no=?   
+                                )
+                            ) AND type=? AND state_election_id = (
+                                SELECT id FROM State_Election WHERE status=1 AND state_code= (
+                                    SELECT state_code FROM Constituency WHERE name=(
+                                        SELECT lok_sabha_constituency FROM Govt_DB WHERE aadhaar_no=?   
+                                    )    
+                                ) AND country_election_id=?
+                            )");
+                            $stmt->bind_param("sssd", $aadhaarNo, $type, $aadhaarNo, $electionId);
+                            $stmt->execute();
+                            $stmt->bind_result($phaseElectionId);
+                            $stmt->fetch();
+                            $stmt->close();
+                        }
+                        else if($type=="VIDHAN SABHA")
+                        {
+                            $response['validType']=true;
+                            $stmt=$conn->prepare("SELECT id FROM Pub_Govt_Election WHERE status=1 AND phase_code = (
+                                SELECT phase_code FROM Constituency WHERE name=(
+                                    SELECT vidhan_sabha_constituency FROM Govt_DB WHERE aadhaar_no=?   
+                                )
+                            ) AND type=? AND state_election_id = (
+                                SELECT id FROM State_Election WHERE status=1 AND id=?
+                            )");
+                            $stmt->bind_param("ssd", $aadhaarNo, $type, $electionId);
+                            $stmt->execute();
+                            $stmt->bind_result($phaseElectionId);
+                            $stmt->fetch();
+                            $stmt->close();
+
+                        }
+
+
+                        if($response['validType'])
+                        {
+                            $count=-1;
+
+                            $stmt=$conn->prepare("SELECT COUNT(election_id) FROM Govt_Vote_Status WHERE election_id=? AND aadhaar_no=?");
+                            $stmt->bind_param("ds", $phaseElectionId, $aadhaarNo);
+                            $stmt->execute();
+                            $stmt->bind_result($count);
+                            $stmt->fetch();
+                            $stmt->close();
+
+                            if($count==0)
+                            {
+                                $response['validVoter']=true;
+                                $count=-1;
+                                
+                                if(sendOTP($conn, $INTERNAL_AUTH_KEY, $countryCode, $regMobNo, $voterOTP, $API_KEY))
+                                    $response['success']=true;
+                            }
+                        }
                     }
                     
             }
