ad6b39d82583744531e28bedbbea9f4754001f04
Arnab Banerjee <35002679+Arnab-Banerjee-Kolkata@users.noreply.github.com>
2020-05-16 22:21:34 +0530

    Arnab | Fixes garbage bug

--- ValidateVoterOtp.php
+++ ValidateVoterOtp.php
@@ -5,7 +5,7 @@ include 'StoreApproval.php';
 include 'Protection.php';
 include 'EncryptionKeys.php';
 
-checkServerIp($INTERNAL_AUTH_KEY);
+//checkServerIp($INTERNAL_AUTH_KEY);
 foreach($_POST as $element)
 {
     checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
