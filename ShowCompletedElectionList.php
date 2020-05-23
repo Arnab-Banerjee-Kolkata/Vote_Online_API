20f3f1af0adf3e7ba409adb96b5bab4c08231dba
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-23 11:13:46 +0530

    Adarsha | Deletes connection error message description

--- ShowCompletedElectionList.php
+++ ShowCompletedElectionList.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ");
 }
 
 
