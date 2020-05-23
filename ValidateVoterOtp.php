1052027629f8676e1985a5dc5c1a63af949f8acc
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-23 11:23:04 +0530

    Adarsha | Deletes connection error message description

--- ValidateVoterOtp.php
+++ ValidateVoterOtp.php
@@ -15,7 +15,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ");
 }
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
