1c1a57f6ca143ef887715414c72c8b9f292ba797
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-23 11:09:26 +0530

    Adarsha | Deletes connection error message description

--- GetPhase.php
+++ GetPhase.php
@@ -14,7 +14,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ");
 }
 
 
